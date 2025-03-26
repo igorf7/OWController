0323d432519c81a4e8df7789723a92a28d7a76b0
Igor Filippov <igorf777@gmail.com>
2025-03-27 01:01:47 +0300

    2025-03-27

--- MCU/src/board/usart.c
+++ MCU/src/board/usart.c
@@ -6,7 +6,7 @@
 #include "usart.h"
 
 static USART_InitTypeDef USART_InitStruct;
-static uint32_t usartClock = 0;
+static uint32_t usartClock = 0;///
 
 /*!
  \brief Initializes the USART module
@@ -14,22 +14,22 @@ static uint32_t usartClock = 0;
  */
 void InitUsart(USART_TypeDef *USARTx)
 {
-    RCC_ClocksTypeDef RCC_Clocks;
+    RCC_ClocksTypeDef RCC_Clocks;///
     GPIO_InitTypeDef GPIO_InitStruct;
     GPIO_TypeDef *OneWirePort;
     uint16_t OneWirePin;
     
-    RCC_GetClocksFreq(&RCC_Clocks);
+    RCC_GetClocksFreq(&RCC_Clocks);///
     
     if (USARTx == USART1) {
-        usartClock = RCC_Clocks.PCLK2_Frequency;
+        usartClock = RCC_Clocks.PCLK2_Frequency;///
         OneWirePin = GPIO_Pin_9;
         OneWirePort = GPIOA;
         RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
         RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
     }
     else if (USARTx == USART2) {
-        usartClock = RCC_Clocks.PCLK1_Frequency;
+        usartClock = RCC_Clocks.PCLK1_Frequency;///
         OneWirePin = GPIO_Pin_2;
         OneWirePort = GPIOA;
         RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
@@ -62,7 +62,46 @@ void InitUsart(USART_TypeDef *USARTx)
  */
 void UsartSetBaudrate(USART_TypeDef *USARTx, uint32_t baudrate)
 {
-    USARTx->BRR = (uint16_t)(usartClock/baudrate);
+//    uint32_t integerdivider = 0;
+//    uint32_t fractionaldivider = 0;
+//    uint32_t tmpreg = 0, apbclock = 0;
+//    RCC_ClocksTypeDef RCC_ClocksStatus;
+//    
+//    uint32_t usartxbase = (uint32_t)USARTx;
+//    
+//    RCC_GetClocksFreq(&RCC_ClocksStatus);
+//    
+//    if (usartxbase == USART1_BASE) {
+//        apbclock = RCC_ClocksStatus.PCLK2_Frequency;
+//    }
+//    else {
+//        apbclock = RCC_ClocksStatus.PCLK1_Frequency;
+//    }
+//  
+//    /* Determine the integer part */
+//    if ((USARTx->CR1 & CR1_OVER8_Set) != 0) {
+//        integerdivider = ((25 * apbclock) / (2 * baudrate));    
+//    }
+//    else {
+//        integerdivider = ((25 * apbclock) / (4 * baudrate));    
+//    }
+//    tmpreg = (integerdivider / 100) << 4;
+
+//    /* Determine the fractional part */
+//    fractionaldivider = integerdivider - (100 * (tmpreg >> 4));
+
+//    /* Implement the fractional part in the register */
+//    if ((USARTx->CR1 & CR1_OVER8_Set) != 0) {
+//        tmpreg |= ((((fractionaldivider * 8) + 50) / 100)) & ((uint8_t)0x07);
+//    }
+//    else {
+//        tmpreg |= ((((fractionaldivider * 16) + 50) / 100)) & ((uint8_t)0x0F);
+//    }
+//  
+//    /* Write to USART BRR */
+//    USARTx->BRR = (uint16_t)tmpreg;
+    
+    USARTx->BRR = (uint16_t)(usartClock/baudrate);///
 }
 
 /*!
